cf7139a608b88cb09f32f48fa9955edc54723111
Ali Reza <hussinialireza9@gmail.com>
2024-05-10 08:53:39 +0430

    commit12

--- split-landing-page/script.js
+++ split-landing-page/script.js
@@ -1,1 +1,14 @@
-'use strict';
+"use strict";
+
+const container = document.querySelector(".container");
+const psEl = document.querySelector(".ps");
+const xboxEl = document.querySelector(".xbox");
+//////////////////////////////
+
+psEl.addEventListener("mouseenter", () => container.classList.add("hover-ps"));
+psEl.addEventListener("mouseout", () => container.classList.remove("hover-ps"));
+
+xboxEl.addEventListener('mouseenter', () => container.classList.add('hover-xbox'));
+xboxEl.addEventListener('mouseout', () => container.classList.remove('hover-xbox'));
+
+
